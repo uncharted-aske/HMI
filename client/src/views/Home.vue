f1642e73fe8ff4b6b967cb0ff7536c39ce33a545
RosaRomeroGomez <rgomez@uncharted.software>
2020-09-15 14:18:13 -0400

    added home and not found views

---
+++ client/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid">
+    TEST
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data: () => ({
+
+  }),
+  mounted () {
+    this.refresh()
+  },
+  methods: {
+
+    refresh () {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
