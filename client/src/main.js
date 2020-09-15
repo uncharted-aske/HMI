80834d2fb061f23012cb14c5847eafb4ac827fb4
Adamo Carolli <acarolli@admins-MacBook-Pro.local>
2020-09-15 18:24:10 -0400

    Add fontawesome to Vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
-
-import router from './router'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import App from './App.vue'
+import router from './router'
+
+library.add(faExclamationTriangle)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
