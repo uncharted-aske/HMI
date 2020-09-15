6232fdf8811ca8a711d10ece3b1c0f47d49eff3d
RosaRomeroGomez <rgomez@uncharted.software>
2020-09-15 16:08:23 -0400

    added bootstrap

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 import NotFound from '@/views/NotFound.vue'
+import BioView from '@/views/BioView.vue'
+import EpiView from '@/views/EpiView.vue'
 
 Vue.use(Router)
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/',
+      name: 'bioView',
+      component: BioView
+    },
+    {
+      path: '/',
+      name: 'epiView',
+      component: EpiView
     },
     /* 404, this has to go last */
     {
