bd81a3b3e5480610fb21976ef6a93c2a19f80e4d
RosaRomeroGomez <rgomez@uncharted.software>
2020-09-15 13:54:41 -0400

    added router folder

---
+++ client/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/views/Home.vue';
+
+Vue.use(Router);
+
+
+export default new Router({
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    /* 404, this has to go last */
+    {
+      path: '*',
+      component: NotFound
+    }
+  ]
+});
