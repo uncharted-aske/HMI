02eb94f010d0936a6dd57552078235a9d45bcbbc
RosaRomeroGomez <rgomez@uncharted.software>
2020-09-16 15:15:20 -0400

    added nav bar component

---
+++ client/src/components/nav-bar.vue
@@ -0,0 +1,25 @@
+<template>
+<nav class="navbar fixed-top navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">App Name</a>
+</nav>
+
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  mounted () {
+    this.refresh()
+  },
+  methods: {
+    refresh () {
+
+    }
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
