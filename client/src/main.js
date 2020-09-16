b12033600dea8762724e44cb82ad2aba369386bd
RosaRomeroGomez <rgomez@uncharted.software>
2020-09-16 14:47:16 -0400

    added css bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-
 import App from './App.vue'
 import router from './router'
 
