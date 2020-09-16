f1724fa9444dd03bbb7256eec912915132ab326c
Dario Segura <dsegura@uncharted.software>
2020-09-16 17:42:07 -0400

    [ESLINT] Config for vue + typescript

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,89 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true
+    browser: true,
+    es2020: true,
+    mocha: true,
+    worker: true,
   },
+
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
   ],
+
   parserOptions: {
-    ecmaVersion: 2020
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
   },
+
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      plugins: [
+        '@typescript-eslint',
+      ],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/no-parameter-properties': 2,
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-var-requires': 2,
+        '@typescript-eslint/no-non-null-assertion': 2,
+        '@typescript-eslint/no-use-before-define': 2,
+        '@typescript-eslint/camelcase': 0,
+        '@typescript-eslint/no-empty-interface': 2,
+        '@typescript-eslint/explicit-function-return-type': 2,
+        '@typescript-eslint/ban-ts-ignore': 0,
+        '@typescript-eslint/no-inferrable-types': [2, {
+          ignoreParameters: true,
+          ignoreProperties: true,
+        }],
+      },
+    },
+
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      plugins: [
+        'vue',
+        '@typescript-eslint',
+      ],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/essential',
+      ],
+      rules: {
+        indent: 'off',
+        'vue/script-indent': ['error', 2, { baseIndent: 1 }],
+        '@typescript-eslint/no-parameter-properties': 2,
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-var-requires': 2,
+        '@typescript-eslint/no-non-null-assertion': 2,
+        '@typescript-eslint/no-use-before-define': 2,
+        '@typescript-eslint/camelcase': 0,
+        '@typescript-eslint/no-empty-interface': 2,
+        '@typescript-eslint/explicit-function-return-type': 2,
+        '@typescript-eslint/ban-ts-ignore': 0,
+        '@typescript-eslint/no-inferrable-types': [2, {
+          ignoreParameters: true,
+          ignoreProperties: true,
+        }],
+        'new-cap': ['error', { capIsNewExceptions: ['Component', 'Prop', 'Watch'] }],
+      },
+    },
+  ],
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-console': process.env.NODE_ENV === 'production' ? 'err' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'err' : 'warn',
+    // TODO: need discussion
+    // enforce comma dangle
+    'comma-dangle': [2, 'always-multiline'],
+    // semicolon stuff
+    'no-extra-semi': 2,
+    'semi-spacing': [1, { before: false, after: true }],
+    semi: [1, 'always'],
+  },
+};
