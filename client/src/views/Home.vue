f8e1c95323d78b1da5048c74629d5314e803c041
Dario Segura <dsegura@uncharted.software>
2020-09-16 21:48:04 -0400

    [CLIENT] Re-added example vue components

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,29 +1,43 @@
 <template>
   <div class="container-fluid">
-     <button
-            type="button"
-            class="btn btn-primary"
-            @click="openView('epiView')"
-          > EPI view</button>
-       <button
-            type="button"
-            class="btn btn-primary"
-            @click="openView('bioView')"
-          > BIO view</button>
+    <button
+      type="button"
+      class="btn btn-primary"
+      @click="openView('epiView')"
+    > EPI view </button>
 
+    <button
+      type="button"
+      class="btn btn-primary"
+      @click="openView('bioView')"
+    > BIO view </button>
+
+    <JSComponent/>
+    <TSComponent/>
+    <TSClassComponent/>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+  import Component from 'vue-class-component';
+  import Vue from 'vue';
 
-export default {
-  name: 'Home',
-  methods: {
-    openView (view) {
-      this.$router.push({ name: view })
+  import JSComponent from '../components/JSComponent.vue';
+  import TSComponent from '../components/TSComponent.vue';
+  import TSClassComponent from '../components/TSClassComponent.vue';
+
+  const components = {
+    JSComponent,
+    TSComponent,
+    TSClassComponent,
+  };
+
+  @Component({ components })
+  export default class Home extends Vue {
+    public openView (view: string): void {
+      this.$router.push({ name: view });
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
