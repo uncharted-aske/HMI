f3e1586859cac56f6b836868d45bd4dea970ecba
RosaRomeroGomez <rgomez@uncharted.software>
2020-09-21 11:52:26 -0400

    added nav bar

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <a href="#" class="navbar-brand">bezKoder</a>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a href="/tutorials" class="nav-link">Tutorials</a>
+        </li>
+        <li class="nav-item">
+          <a href="/add" class="nav-link">Add</a>
+        </li>
+      </div>
+    </nav>
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  export default class NavBar extends Vue {}
+</script>
