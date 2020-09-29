581f02c8c21a0df2527a8154f7637fd148414ff8
RosaRomeroGomez <rgomez@uncharted.software>
2020-09-29 11:31:33 -0400

    lint issues

--- client/src/components/ActionColumn.vue
+++ client/src/components/ActionColumn.vue
@@ -14,8 +14,6 @@
     components: {
       LeftSidePanel,
     },
-    data: () => ({
-    }),
     methods: {
 
     },
---
+++ client/src/components/widgets/CloseButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="close-button"
+    @click="$emit('click')"
+  >
+    <font-awesome-icon :icon="['fas', 'times']" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CloseButton',
+  };
+</script>
+
+<style lang="scss" scoped>
+
+.close-button {
+  font-size: 16px;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 3px;
+  cursor: pointer;
+
+  &:hover {
+    color: red;
+  }
+
+  position: absolute;
+  top: 12px;
+  right: 12px;
+}
+</style>
