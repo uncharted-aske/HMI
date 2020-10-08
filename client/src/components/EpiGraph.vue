f3a7ce352c52d6be087e962ce0a22f8757fb1991
RosaRomeroGomez <rgomez@uncharted.software>
2020-10-08 12:54:06 -0400

    added epi graph component

---
+++ client/src/components/EpiGraph.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="epi-graph-container">
+      GRAPH
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class EpiGraph extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
