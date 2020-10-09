dedfc0cfbbe553ad01315f0b9f4831dd4a9562aa
RosaRomeroGomez <rgomez@uncharted.software>
2020-10-09 15:59:37 -0400

    added counters component

---
+++ client/src/components/Counters.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="counters-container">
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class Counters extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+
+.counters-container {
+  height: calc(#{$secondary-bar-width} - 15px);
+  background-color: $secondary-bar-bg;
+  border: 1px solid rgba(207, 216, 220, .5);
+}
+
+
+</style>
