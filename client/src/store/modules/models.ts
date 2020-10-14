6e83dcc464e00e95ae5698bcddc8c370656a97a9
RosaRomeroGomez <rgomez@uncharted.software>
2020-10-14 14:03:04 -0400

    added SIR

--- client/src/store/modules/models.ts
+++ client/src/store/modules/models.ts
@@ -2,6 +2,8 @@ import { GetterTree, MutationTree } from 'vuex';
 import { ModelsState } from '../../types/types';
 
 import CHIME from '../../assets/uncharted_chime.json';
+import SIR from '../../assets/uncharted_sir.json';
+
 
 const state: ModelsState = {
   selectedModelId: null,
@@ -10,6 +12,12 @@ const state: ModelsState = {
       id: 1,
       metadata: CHIME.metadata,
       graph: { nodes: CHIME.nodes, edges: CHIME.edges, groups: CHIME.groups },
+      type: 'computational',
+    },
+    {
+      id: 2,
+      metadata: SIR.metadata,
+      graph: { nodes: SIR.nodes, edges: SIR.edges, groups: SIR.groups },
       type: 'computational',
     },
   ],
