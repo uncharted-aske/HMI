84d5022cb3cd3470f1d812e7b0e6aebd395058f2
RosaRomeroGomez <rgomez@uncharted.software>
2020-10-14 15:02:11 -0400

    added metadata pane on drilldown

---
+++ client/src/components/DrilldownMetadataPane.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="drilldown-metadata-pane-container">
+      {{metadata}}
+    <!-- <div class="metadata-item">
+      <div class="key">Name</div>
+      <div>{{metadata.name}}</div>
+    </div>
+    <div class="metadata-item">
+       <div class="key">Description</div>
+      <div>{{metadata.description}}</div>
+    </div>
+    <div class="metadata-item">
+       <div class="key">Created</div>
+      <div>{{metadata.created}}</div>
+    </div>
+    <div class="metadata-item">
+       <div class="key">Source</div>
+      <div>{{metadata.source}}</div>
+    </div>
+    <div class="metadata-item">
+       <div class="key">Version</div>
+      <div>{{metadata.version}}</div>
+    </div>
+    <div class="metadata-item">
+       <div class="key">Knowledge</div>
+      <div><a class="text-link" :href="metadata.knowledge">{{metadata.knowledge}}</a> </div>
+    </div> -->
+  </div>
+</template>
+
+<script lang="ts">
+  import Component from 'vue-class-component';
+  import Vue from 'vue';
+  import { Prop } from 'vue-property-decorator';
+
+  import { ModelComponentMetadataInterface } from '../types/types';
+
+  @Component
+  export default class DrilldownMetadataPane extends Vue {
+    @Prop({ default: null }) metadata: ModelComponentMetadataInterface;
+  }
+</script>
+
+<style lang="scss" scoped>
+.metadata-item {
+  padding: 20px;
+  text-align: left;
+  border-bottom: 1px solid rgba(207, 216, 220, .5);
+  .key {
+    font-weight: bold;
+    padding-top: 5px;
+  }
+}
+</style>
