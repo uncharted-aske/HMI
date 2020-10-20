27ee4c9f055693f4925b91df2cdc99053dc4463e
RosaRomeroGomez <rgomez@uncharted.software>
2020-10-20 12:43:32 -0400

    added snippets

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,109 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div
+        class="modal-wrapper"
+        @click.stop="close()">
+        <div
+          class="modal-container"
+        >
+
+          <div
+            class="modal-header"
+          >
+            <slot name="header" />
+            <close-button
+              @click="close()"
+            />
+          </div>
+
+          <div class="modal-body">
+            <slot name="body" />
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+/**
+ * Simple modal wrapper
+*/
+import Component from 'vue-class-component';
+import Vue from 'vue';
+
+import CloseButton from '@/components/widgets/CloseButton.vue';
+
+const components = {
+  CloseButton
+};
+
+@Component({ components })
+export default class Modal extends Vue {
+  close(): void {
+    this.$emit('close', null);
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+
+.modal-mask {
+  position: fixed;
+  z-index: map-get($z-index-order, modal);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  position: relative;
+  width: 500px;
+  margin: 0px auto;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+}
+
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+</style>
---
+++ client/src/components/ModalKnowledge.vue
@@ -0,0 +1,46 @@
+<template>
+  <modal
+    @close="close()"
+  >
+    <h4 slot="header"> CHIME</h4>
+    <div
+      slot="body"
+    >
+    </div>
+  </modal>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component';
+import Vue from 'vue';
+
+import Modal from '@/components/Modal.vue';
+
+// import ChimePDF from '@/assets/CHIME.pdf';
+
+const components = {
+  Modal,
+  // pdf
+};
+
+@Component({ components })
+export default class ModalKnowledge extends Vue {
+  // chimePDF: string = ChimePDF;
+
+  close(): void {
+    this.$emit('close', null);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+img {
+        // Clip images that are too big, but maintain aspect ratio
+        object-fit: cover;
+        width: 100%;
+        height: 100%
+      }
+
+
+</style>
