dd38e91945e03ea8d06d93d5ff6bf4686a39e71d
RosaRomeroGomez <rgomez@uncharted.software>
2020-10-20 12:43:44 -0400

    added some styling

--- client/src/components/DrilldownMetadataPane.vue
+++ client/src/components/DrilldownMetadataPane.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="drilldown-metadata-pane-container" v-if="metadata">
     <div v-for="(value, key) in metadata" :key="key" class="metadata-item">
-      <div class="key"> {{key | capital-letters-formatter | underscore-remover-formatter}} </div>
-      <div :class="{'expression': key === 'expression' || key === 'eqn_source', '': key !== 'expression' || key !== 'eqn_source' }">{{value}} </div>
-     </div>
+      <div v-if="key !== 'knowledge'">
+        <div class="key" v-if="key !== 'knowledge'"> {{key | capital-letters-formatter | underscore-remover-formatter}} </div>
+        <div :class="{'expression': key === 'expression' || key === 'eqn_source', '': key !== 'expression' || key !== 'eqn_source' }">{{value}} </div>
+      </div>
+     <div v-else>
+       <div class="key"> {{key | capital-letters-formatter | underscore-remover-formatter}} </div>
+       <div class="snippet" @click="showModal = true">
+        <img :src="firstSnippet" />
+       </div>
+       <div class="snippet" @click="showModal = true"> <img :src="secondSnippet" /></div>
+     </div> 
+    </div>
+    <modal-knowledge
+      v-if="showModal"
+      @close="showModal = false"
+     />
   </div>
-      <div v-else>
-       Empty metadata
-     </div>
 </template>
 
 <script lang="ts">
@@ -15,9 +25,22 @@
   import Vue from 'vue';
   import { Prop } from 'vue-property-decorator';
 
-  @Component
+  import ModalKnowledge from '@/components/ModalKnowledge.vue';
+
+  import FirstSnippet from '@/assets/img/SnippetKnowledge1.png';
+  import SecondSnippet from '@/assets/img/SnippetKnowledge2.png';
+
+  const components = {
+    ModalKnowledge
+  };
+
+  @Component({ components })
   export default class DrilldownMetadataPane extends Vue {
     @Prop({ default: null }) metadata: any;
+
+    showModal: boolean = false;
+    firstSnippet: string = FirstSnippet;
+    secondSnippet: string = SecondSnippet;
   }
 </script>
 
@@ -31,6 +54,18 @@
   }
   .expression {
       font-family:Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New;
+  }
+  .snippet {
+    width: 100%;
+    height: 100px;
+    border: 1px solid rgba(207, 216, 220, .5);
+    margin: 5px;
+     img {
+        // Clip images that are too big, but maintain aspect ratio
+        object-fit: cover;
+        width: 100%;
+        height: 100%
+      }
   }
 }
 </style>
