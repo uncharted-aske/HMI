b55edcf6179cd1aca5400a75211a1511a81dace3
RosaRomeroGomez <rgomez@uncharted.software>
2020-11-10 12:08:39 -0500

    moved calc neighborhood to the graph util

--- client/src/graphs/svg/util.js
+++ client/src/graphs/svg/util.js
@@ -1,3 +1,9 @@
+
+export const NODE_TYPES = {
+  VARIABLE: 'variable',
+  FUNCTION: 'function'
+}
+
 /**
  * Recursively traverse a graph that looks like
  * {
@@ -102,3 +108,26 @@ export const formatHierarchyNodeData = (root) => {
     }
   }
 };
+
+
+/**
+ * Get the neighborhood graph for a selected node
+ * @param {object} graph - an object of nodes/edges arrays
+ * @param {string} node - a node id
+ */
+
+export const calculateNeighborhood = (graph, node) => {
+  const neighborEdges = graph.edges.filter(edge => {
+    return edge.target === node || edge.source === node;
+  }).map(edge => {
+    return { source: edge.source, target: edge.target };
+  });
+
+  // Reverse-engineer nodes from edges
+  const neighborNodes = _.uniq(_.flatten(neighborEdges.map(edge => {
+    return [edge.source, edge.target];
+  })).concat(node)).map(id => ({ id })); // Include the selected node (added into .uniq)
+
+  return { nodes: neighborNodes, edges: neighborEdges };
+}
+
