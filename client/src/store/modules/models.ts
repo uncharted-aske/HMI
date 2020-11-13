09f7128c5f10c699ced0eb70ef75fd3b933dca09
RosaRomeroGomez <rgomez@uncharted.software>
2020-11-13 16:06:59 -0500

    added CHIME and the double epidemic one

--- client/src/store/modules/models.ts
+++ client/src/store/modules/models.ts
@@ -4,6 +4,7 @@ import { GetterTree, MutationTree } from 'vuex';
 import { ModelsState } from '../../types/types';
 
 import CHIME from '../../static/uncharted_chime.json';
+import nestedCHIMECAG from '../../static/nested-CHIME-SIR-CAG.json';
 import nestedCHIMEGrFN from '../../static/nested-CHIME-SIR-GrFN.json';
 
 import SIR from '../../static/uncharted_sir.json';
@@ -11,6 +12,7 @@ import nestedSIRCAG from '../../static/nested-SIR-simple-CAG.json';
 import nestedSIRGrFN from '../../static/nested-SIR-simple-GrFN.json';
 
 import DoubleEpi from '../../static/uncharted_double_epi.json';
+import nestedDoubleEpiCAG from '../../static/nested-SARS-COV1-SEIRP-CAG.json';
 import nestedDoubleEpiGrFN from '../../static/nested-SARS-COV1-SEIRP-GrFN.json';
 
 const state: ModelsState = {
@@ -29,7 +31,7 @@ const state: ModelsState = {
       id: 2,
       metadata: CHIME.metadata,
       graph: {
-        abstract: _.pick(nestedCHIMEGrFN, ['nodes', 'edges']),
+        abstract: _.pick(nestedCHIMECAG, ['nodes', 'edges']),
         detailed: _.pick(nestedCHIMEGrFN, ['nodes', 'edges']),
       },
       type: 'computational',
@@ -38,7 +40,7 @@ const state: ModelsState = {
       id: 3,
       metadata: DoubleEpi.metadata,
       graph: {
-        abstract: _.pick(nestedDoubleEpiGrFN, ['nodes', 'edges']),
+        abstract: _.pick(nestedDoubleEpiCAG, ['nodes', 'edges']),
         detailed: _.pick(nestedDoubleEpiGrFN, ['nodes', 'edges']),
       },
       type: 'computational',
