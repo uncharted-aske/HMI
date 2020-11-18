a610ee5a9d5b03663dbd08bf7b2fc8cffbe2c1cc
RosaRomeroGomez <rgomez@uncharted.software>
2020-11-18 12:09:01 -0500

    removed unneeded imports in the library

--- client/src/views/Graphs/views/Epi/components/EpiGraph/EpiGraph.vue
+++ client/src/views/Graphs/views/Epi/components/EpiGraph/EpiGraph.vue
@@ -10,7 +10,7 @@
 
   import { GraphInterface } from '@/views/Graphs/types/types';
 
-  // import EpiModelRenderer from '../../../../../../graphs/svg/EpiModelRenderer.js';
+  // import EpiModelRenderer from '@/graphs/svg/EpiModelRenderer';
   // import ELKAdapter from '../../../../../../graphs/svg/elk/adapter.js';
   // import { layered } from '../../../../../../graphs/svg//elk/layouts.js';
   // import { hierarchyFn } from '../../../../../../utils/SVGUtil.js';
@@ -22,83 +22,83 @@
   //   layout: layered,
   // };
 
-  // @Component
-  // export default class EpiGraph extends Vue {
-  //   @Prop({ default: null }) graph: GraphInterface;
+  @Component
+  export default class EpiGraph extends Vue {
+    @Prop({ default: null }) graph: GraphInterface;
 
-  //   renderingOptions = DEFAULT_RENDERING_OPTIONS;
-  //   renderer = null;
+    // renderingOptions = DEFAULT_RENDERING_OPTIONS;
+    // renderer = null;
 
-  //   @Watch('graph')
-  //   graphChanged (): void {
-  //     this.refresh();
-  //   }
+    // @Watch('graph')
+    // graphChanged (): void {
+    //   this.refresh();
+    // }
 
-  //   created (): void {
-  //     this.renderer = null;
-  //   }
+    // created (): void {
+    //   this.renderer = null;
+    // }
 
-  //   mounted (): void {
-  //     this.renderer = new EpiModelRenderer({
-  //       el: this.$refs.graph,
-  //       adapter: new ELKAdapter(DEFAULT_RENDERING_OPTIONS),
-  //       renderMode: 'delta',
-  //     });
+    mounted (): void {
+    //   this.renderer = new EpiModelRenderer({
+    //     el: this.$refs.graph,
+    //     adapter: new ELKAdapter(DEFAULT_RENDERING_OPTIONS),
+    //     renderMode: 'delta',
+    //   });
 
-  //     this.renderer.setCallback('nodeClick', (node) => {
-  //       // this.$emit('node-click', node.datum().data); // TO FIX
-  //       const neighborhood = calculateNeighborhood(this.graph, node.datum().id);
-  //       this.renderer.showNeighborhood(neighborhood);
-  //     });
+    //   this.renderer.setCallback('nodeClick', (node) => {
+    //     // this.$emit('node-click', node.datum().data); // TO FIX
+    //     const neighborhood = calculateNeighborhood(this.graph, node.datum().id);
+    //     this.renderer.showNeighborhood(neighborhood);
+    //   });
 
-  //    // Collapse/Expand
-  //    this.renderer.setCallback('nodeDblClick', (node) => {
-  //       if (!node.datum().collapsed || node.datum().collapsed === false) {
-  //         this.renderer.collapse(node.datum().id);
-  //       } else {
-  //         this.renderer.expand(node.datum().id);
-  //       }
-  //     });
+    //  // Collapse/Expand
+    //  this.renderer.setCallback('nodeDblClick', (node) => {
+    //     if (!node.datum().collapsed || node.datum().collapsed === false) {
+    //       this.renderer.collapse(node.datum().id);
+    //     } else {
+    //       this.renderer.expand(node.datum().id);
+    //     }
+    //   });
 
-  //     this.renderer.setCallback('backgroundDblClick', () => {
-  //       this.renderer.hideNeighbourhood();
-  //     });
+    //   this.renderer.setCallback('backgroundDblClick', () => {
+    //     this.renderer.hideNeighbourhood();
+    //   });
 
-  //     // TO FIX:
-  //     // this.renderer.setCallback('nodeMouseEnter', (node) => {
-  //     //   const nodeData = node.datum();
-  //     //   let nodeCoords = [];
-  //     //   const tooltipText = 'Name: ' + nodeData.label + ' ' + 'Type: ' + nodeData.data.nodeType;
-  //     //   // if (!nodeData.nodes) {
-  //     //   //   // console.log(nodeData);
-  //     //   //   nodeCoords = [nodeData.x, nodeData.y];
-  //     //   //   console.log(nodeCoords);
-  //     //   // }
-  //     //   // else {
-  //     //     // // For nodes inside groups
-  //     //     // const groups = this.renderer.layout.groups;
-  //     //     // const group = groups.find(g => g.id === nodeData.group);
-  //     //     // nodeCoords = [group.x + nodeData.x, group.y + nodeData.y];
-  //     //   //}
-  //     //   showTooltip(this.renderer.chart, tooltipText, nodeCoords);
-  //     // });
+      // TO FIX:
+      // this.renderer.setCallback('nodeMouseEnter', (node) => {
+      //   const nodeData = node.datum();
+      //   let nodeCoords = [];
+      //   const tooltipText = 'Name: ' + nodeData.label + ' ' + 'Type: ' + nodeData.data.nodeType;
+      //   // if (!nodeData.nodes) {
+      //   //   // console.log(nodeData);
+      //   //   nodeCoords = [nodeData.x, nodeData.y];
+      //   //   console.log(nodeCoords);
+      //   // }
+      //   // else {
+      //     // // For nodes inside groups
+      //     // const groups = this.renderer.layout.groups;
+      //     // const group = groups.find(g => g.id === nodeData.group);
+      //     // nodeCoords = [group.x + nodeData.x, group.y + nodeData.y];
+      //   //}
+      //   showTooltip(this.renderer.chart, tooltipText, nodeCoords);
+      // });
 
-  //     // this.renderer.setCallback('nodeMouseLeave', () => {
-  //     //   hideTooltip(this.renderer.chart);
-  //     // });
+      // this.renderer.setCallback('nodeMouseLeave', () => {
+      //   hideTooltip(this.renderer.chart);
+      // });
 
-  //     this.refresh();
-  //   }
+      this.refresh();
+    }
 
-  //   refresh (): void {
-  //     const hierarchyNodes = hierarchyFn(this.graph.nodes); // Transform the flat nodes structure into a hierarchical one
-  //     formatHierarchyNodeData(hierarchyNodes); // Refines this hierarchical structure to a format that can be used by the renderer
-  //     const edges = this.graph.edges;
-  //     const graph = { nodes: [hierarchyNodes], edges };
-  //     this.renderer.setData(graph);
-  //     this.renderer.render();
-  //   }
-  // }
+    refresh (): void {
+      // const hierarchyNodes = hierarchyFn(this.graph.nodes); // Transform the flat nodes structure into a hierarchical one
+      // formatHierarchyNodeData(hierarchyNodes); // Refines this hierarchical structure to a format that can be used by the renderer
+      // const edges = this.graph.edges;
+      // const graph = { nodes: [hierarchyNodes], edges };
+      // this.renderer.setData(graph);
+      // this.renderer.render();
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
