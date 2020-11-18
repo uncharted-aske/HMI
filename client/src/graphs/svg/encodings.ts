297ee50940afeca88f631f53a859fd36cbc1996c
RosaRomeroGomez <rgomez@uncharted.software>
2020-11-18 17:54:37 -0500

    lint issues

--- client/src/graphs/svg/encodings.ts
+++ client/src/graphs/svg/encodings.ts
@@ -2,13 +2,15 @@
  * Nodes and edges color mappings
  */
 export abstract class Colors {
-  static readonly VARIABLES: object = {
-    MODEL_VARIABLE: '#0091EA',
-    PARAMETER: '#FFC04D',
-  };;
-  static readonly NODES: object = {
+  static readonly VARIABLES: Record<string, any> = {
     MODEL_VARIABLE: '#0091EA',
     PARAMETER: '#FFC04D',
   };
+
+  static readonly NODES: Record<string, any> = {
+    MODEL_VARIABLE: '#0091EA',
+    PARAMETER: '#FFC04D',
+  };
+
   static readonly EDGES: string = '#6C757D';
 }
