95d03f0810801393d60605f110a48f619c7dd142
RosaRomeroGomez <rgomez@uncharted.software>
2020-11-19 11:32:51 -0500

    lint issues

--- client/src/graphs/svg/encodings.ts
+++ client/src/graphs/svg/encodings.ts
@@ -4,7 +4,7 @@
 export abstract class Colors {
   static readonly NODES: Record<string, any> = {
     DEFAULT: '#EEEEEE',
-    CONTAINER:'#F8F8F8',
+    CONTAINER: '#F8F8F8',
     MODEL_VARIABLE: '#0091EA',
     PARAMETER: '#FFC04D',
   };
