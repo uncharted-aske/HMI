7580ae9c0ada34fe874e127d7a4e41082d34b39d
RosaRomeroGomez <rgomez@uncharted.software>
2020-12-08 18:58:14 +0100

    added generic settings bar

---
+++ client/src/components/SettingsBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="settings-bar-container">
+    <div class="counters">
+        <slot name="counters" />
+    </div>
+    <div class="settings">
+        <slot name="settings" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class Counters extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/variables";
+
+.settings-bar-container {
+  height: calc(#{$secondary-bar-width} - 15px);
+  background-color: $secondary-bar-bg;
+  border: 1px solid rgba(207, 216, 220, .5);
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  justify-content: space-between;
+}
+
+</style>
