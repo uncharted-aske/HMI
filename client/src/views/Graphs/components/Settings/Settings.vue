73dfa660ee5d91f53c9ed147e62ad8ffb4345755
RosaRomeroGomez <rgomez@uncharted.software>
2020-12-08 20:45:23 +0100

    added settings component

---
+++ client/src/views/Graphs/components/Settings/Settings.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="settings-container">
+      
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class Settings extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/variables";
+
+.settings-container {
+  display: flex;
+  align-items: center;
+}
+
+</style>
