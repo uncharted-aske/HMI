d78f1e1b394de27e98271aa01f121e3ca68fa73c
RosaRomeroGomez <rgomez@uncharted.software>
2021-01-29 17:09:38 -0500

    added color to the header

---
+++ client/src/components/CollapsibleItem.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="collapsible-container">
+    <div class="item-container">
+      <div
+        class="item-title"
+        @click="toggle()">
+        <font-awesome-icon class="icon" :icon="['fas', getIcon]"/>
+        <slot name="title" />
+      </div>
+    </div>
+    <div v-if="expanded === true">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import _ from 'lodash';
+
+import Component from 'vue-class-component';
+import Vue from 'vue';
+import { Prop } from 'vue-property-decorator';
+
+/**
+ * A collapsible wrapper component that allows the injection of two external
+ * fragments into the template slots.
+ *
+ * - title: title bar elements
+ * - content: elements to be displayed
+ *
+ * For example:
+ *   <collapsible-item>
+ *      <div slot="title"> This replaces the header slot in template</div>
+ *      <p slot="content">
+ *          This paragraph replaces the content slot in template
+ *          <img src="something"/>
+ *      </p>
+ *   </collapsible-item>
+ *
+*/
+
+  @Component
+  export default class CollapsibleItem extends Vue {
+
+    @Prop({ default: false }) defaultExpand: boolean;
+    expanded: boolean = false; 
+
+    get getIcon():string {
+        return this.expanded ? 'caret-up' : 'caret-down';
+    }
+
+    expand(): void {
+      this.expanded = true;
+    }
+
+    collapse(): void {
+      this.expanded = false;
+    }
+
+    toggle(): void {
+      this.expanded = !this.expanded;
+    }  
+
+  }
+
+</script>
+
+<style lang="scss">
+@import "@/styles/variables";
+.collapsible-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 2px 0;
+
+  .item-container {
+    display: flex;
+    flex-direction: row;
+    padding: 2px 0;
+    .item-controls {
+      align-items: baseline;
+      flex: 1;
+      margin-top: auto;
+      margin-bottom: auto;
+    }
+    .item-title {
+      display: flex;
+      flex-direction: row;
+      align-items: baseline;
+      padding: 5px;
+      cursor: pointer;
+      flex: 16;
+      min-width: 0px;
+      background-color: $drilldown-header;
+    //   .icon {
+    //     color: $icon-color-dark;
+    //     padding: 5px;
+    //   }
+    }
+  }
+}
+
+
+</style>
---
+++ client/src/views/Graphs/views/Epi/components/DrilldownMetadataPanel/DrilldownMetadataPane.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="drilldown-metadata-pane-container">
+    <div v-if="!isEmptyMetadata" >
+
+      <collapsible-item>
+        <div slot="title">Type</div>
+        <div slot="content">
+              {{getType}}
+        </div>
+      </collapsible-item>
+
+      <collapsible-item>
+        <div slot="title">Provenance</div>
+        <div slot="content">
+          <div class="metadata-item">
+          <div v-for="(value, key) in getProvenance" :key="key">
+            <div class="key">{{key | capitalize-first-letter-formatter}}</div>
+            <div v-if="key !== 'sources'">{{value}}</div>
+            <div v-else> 
+              <div v-for="(source, key) in value[0]" :key="key">
+                  {{source}}
+                </div> 
+            </div>
+          </div> 
+        </div>
+        </div>
+      </collapsible-item>
+
+      <collapsible-item>
+        <div slot="title">Attributes</div>
+        <div slot="content">
+        <div class="metadata-item">
+          <div v-for="(item, index) in getAttributes" :key="index">
+            <div v-for="(value, key) in item" :key="key">
+            <div class="key">{{key | capitalize-first-letter-formatter}}</div>
+              {{value}}
+            </div> 
+          </div>   
+        </div>      </div>
+      </collapsible-item>
+
+    </div>
+
+    <div v-else class="alert alert-info" role="alert">
+      No metadata at the moment
+    </div>
+    <!-- <modal-knowledge
+      v-if="showModal"
+      @close="showModal = false"
+     /> -->
+  </div>
+</template>
+
+<script lang="ts">
+  import _ from 'lodash';
+
+  import Component from 'vue-class-component';
+  import Vue from 'vue';
+  import { Prop } from 'vue-property-decorator';
+
+  import CollapsibleItem from '@/components/CollapsibleItem.vue';
+
+  const components = {
+    CollapsibleItem,
+  };
+
+  @Component({ components })
+  export default class DrilldownMetadataPane extends Vue {
+    @Prop({ default: null }) metadata: any;
+
+    showModal: boolean = false;
+    // type: string = '';
+    // provenance: String[] = [];
+    
+    get isEmptyMetadata (): boolean {
+      return _.isEmpty(this.metadata);
+    }
+
+    get getType (): string {
+      return !_.isEmpty(this.metadata) && this.metadata.type;
+    }
+    get getProvenance (): any {
+      return !_.isEmpty(this.metadata) && this.metadata.provenance;
+    }
+
+    get getAttributes (): any {
+      return !_.isEmpty(this.metadata) && this.metadata.attributes;
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/variables";
+
+.drilldown-metadata-pane-container {
+  padding: 5px;
+  .metadata-header {
+    padding: 5px;
+    color: $text-color-dark;
+    background-color: $drilldown-header;
+  }
+  .metadata-item {
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+    hyphens: auto;
+    text-align: left;
+    margin-top: 5px;
+    padding: 5px;
+    .key {
+      font-weight: bold;
+      padding-top: 5px;
+    }
+
+    //FIXME: Put back when we have document artifacts from Cosmosonsin
+    // .expression {
+    //     font-family:Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New;
+    // }
+    // .snippet {
+    //   width: 100%;
+    //   height: 100px;
+    //   border: 1px solid rgba(207, 216, 220, .5);
+    //   margin: 5px;
+    //   img {
+    //       // Clip images that are too big, but maintain aspect ratio
+    //       object-fit: cover;
+    //       width: 100%;
+    //       height: 100%
+    //     }
+    //   }
+    }
+}
+</style>
