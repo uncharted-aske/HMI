3170e270d27bf8e2651c0724aeb26a55b4e886ec
Dario Segura <dsegura@uncharted.software>
2021-02-02 11:53:33 -0500

    Cleaned unused code

--- client/src/views/Graphs/views/Bio/components/Grafer.vue
+++ client/src/views/Graphs/views/Bio/components/Grafer.vue
@@ -17,31 +17,6 @@
     private controller: GraferController;
 
     public mounted (): void {
-      const nodes = {
-        data: [
-          { x: -8.6, y: 5.0 },
-          { x: 8.6, y: 5.0 },
-          { x: 0.0, y: -10.0 },
-          { x: 0.0, y: 0.0 },
-        ],
-      };
-
-      const edges = {
-        data: [
-          { source: 0, target: 1 },
-          { source: 1, target: 2 },
-          { source: 2, target: 0 },
-
-          { source: 3, target: 0 },
-          { source: 3, target: 1 },
-          { source: 3, target: 2 },
-        ],
-      };
-
-      const layers = [
-        { nodes, edges },
-      ];
-
       this.loadGraph().then(data => {
         this.controller = new GraferController(this.$refs.canvas, data);
         this.loading = false;
