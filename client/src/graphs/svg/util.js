f081bd4e4fba4fa8f18b3c056e551b82d28b4dc0
RosaRomeroGomez <rgomez@uncharted.software>
2021-03-23 18:10:14 -0400

    fixed issue with formatter

--- client/src/graphs/svg/util.js
+++ client/src/graphs/svg/util.js
@@ -105,17 +105,15 @@ export const changeKey = (obj, before, after) => {
  */
 
 export const constructRootNode = (root) => {
-  // if (root.children || root.nodes) {
     root.label = root.data.label;
     root.concept = root.data.concept;
     changeKey(root, 'children', 'nodes');
 
-    // if (root.children) {
+    if (root.nodes) {
       for (let i = 0; i < root.nodes.length; i++) {
         constructRootNode(root.nodes[i]);
       }
-    //}
-  //}
+    }
 };
 
 /**
