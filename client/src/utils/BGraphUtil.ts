ab1d41b5a6bd8401fdc3d39be1f419246e6f2208
Adamo Carolli <adamo.carolli@gmail.com>
2021-03-26 17:41:23 -0400

    [BGRAPH][FILE_LOADER] Fetch and parse gzipped bgraph files

--- client/src/utils/BGraphUtil.ts
+++ client/src/utils/BGraphUtil.ts
@@ -1,5 +1,6 @@
 import s3Client from '@/services/S3Service';
 import { loadJSONLFile } from '@/utils/FileLoaderUtil';
+import { DataFile } from '@dekkai/data-source';
 
 // FIXME: Fix return type once bgraph library types are added
 export const loadBGraphData = async (): Promise<any[]> => {
@@ -7,13 +8,21 @@ export const loadBGraphData = async (): Promise<any[]> => {
     Bucket: process.env.S3_BUCKET,
     Key: process.env.S3_BGRAPH_EDGES_KEY,
   });
-  const bgEdges = await loadJSONLFile(getSignedBGraphEdgesUrl);
+  // TODO: @dekkai/data-source is unable to properly stream in gzipped files so we
+  // are using a workaround by fetching a blob until the following issue is fixed:
+  // https://github.com/dekkai-data/data-source/issues/1
+  const bgEdgesBlob = await (await fetch(getSignedBGraphEdgesUrl)).blob();
+  const bgEdges = await loadJSONLFile(await DataFile.fromLocalSource(bgEdgesBlob));
 
   const getSignedBGraphNodesUrl = s3Client.getSignedUrl('getObject', {
     Bucket: process.env.S3_BUCKET,
     Key: process.env.S3_BGRAPH_NODES_KEY,
   });
-  const bgNodes = await loadJSONLFile(getSignedBGraphNodesUrl);
+  // TODO: @dekkai/data-source is unable to properly stream in gzipped files so we
+  // are using a workaround by fetching a blob until the following issue is fixed:
+  // https://github.com/dekkai-data/data-source/issues/1
+  const bgNodesBlob = await (await fetch(getSignedBGraphNodesUrl)).blob();
+  const bgNodes = await loadJSONLFile(await DataFile.fromLocalSource(bgNodesBlob));
 
   return [bgNodes, bgEdges];
 };
