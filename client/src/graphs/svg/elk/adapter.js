68b8107033d087fd42d4a75c6fc2b79b054afbd5
RosaRomeroGomez <rgomez@uncharted.software>
2021-04-08 17:31:17 -0400

    reset adapter

--- client/src/graphs/svg/elk/adapter.js
+++ client/src/graphs/svg/elk/adapter.js
@@ -74,12 +74,9 @@ const injectELKOptions = (renderGraph, options) => {
     }
     node.ports = ports;
 
-    const nodeSizeScale = options.nodeSizeScale;
     if (!node.nodes || node.nodes.length === 0) {
-      const degree = node.data.in_degree + node.data.out_degree;
-      console.log(degree);
-      node.width = nodeSizeScale(degree) || options.nodeWidth;
-      node.height = nodeSizeScale(degree) * 0.5 || options.nodeHeight;
+      node.width = node.width || options.nodeWidth;
+      node.height = node.height || options.nodeHeight;
     } else {
       delete node.width;
       delete node.height;
