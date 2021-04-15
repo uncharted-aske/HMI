fbc36e160e60a764d36baf12d6087912fcdb5d29
Adamo Carolli <adamo.carolli@gmail.com>
2021-04-15 17:06:34 -0400

    [COLORS][GRAFER] Create utility for colors and add notes to colors used in Grafer

--- client/src/utils/GraferUtil.ts
+++ client/src/utils/GraferUtil.ts
@@ -1,15 +1,19 @@
+import { COLORS } from '@/utils/colorsUtil';
+
 export const BIO_GRAPH_COLORS = [
-  '#5e81ac',
-  '#d08770',
-  '#ebcb8b',
-  '#81a1c1',
+  COLORS.FROST_4_DEEP_ARCTIC_OCEAN, // Used for edges between clusters (inter edges)
+  COLORS.AURORA_2_ORANGE, // Used for nodes
+  COLORS.AURORA_3_YELLOW, // Used for edges within clusters (intra edge)
+  COLORS.FROST_3_ARCTIC_OCEAN, // Used for cluster rings
 ];
 
 export const CLUSTER_GRAPH_COLORS = [
-  '#bf616a',
-  '#d08770',
-  '#ebcb8b',
-  '#a3be8c',
-  '#b48ead',
-  '#d8dee9',
+  COLORS.SNOW_STORM_1, // Used for non-highlighted node labels
+  // NOTE: Since there are more clusters then colors these colors do not map to any one particular
+  //       cluster but may be used in multiple clusters
+  COLORS.AURORA_1_RED, // Used for highlighted nodes labels and as one of various background node colors
+  COLORS.AURORA_2_ORANGE, // Used for nodes to signify cluster relationship with other nodes
+  COLORS.AURORA_3_YELLOW, // Used for nodes to signify cluster relationship with other nodes
+  COLORS.AURORA_4_GREEN, // Used for nodes to signify cluster relationship with other nodes
+  COLORS.AURORA_5_PURPLE, // Used for nodes to signify cluster relationship with other nodes
 ];
---
+++ client/src/utils/colorsUtil.ts
@@ -0,0 +1,19 @@
+export enum COLORS {
+  // See FROST color palette here: https://www.nordtheme.com/docs/colors-and-palettes
+  FROST_1_FROZEN_POLAR_WATER = '#8fbcbb',
+  FROST_2_CLEAR_ICE = '#88c0d0',
+  FROST_3_ARCTIC_OCEAN = '#81a1c1',
+  FROST_4_DEEP_ARCTIC_OCEAN = '#5e81ac',
+
+  // See AURORA color palette here: https://www.nordtheme.com/docs/colors-and-palettes
+  AURORA_1_RED = '#bf616a',
+  AURORA_2_ORANGE = '#d08770',
+  AURORA_3_YELLOW = '#ebcb8b',
+  AURORA_4_GREEN = '#a3be8c',
+  AURORA_5_PURPLE = '#b48ead',
+
+  // See SNOW STORM color palette here: https://www.nordtheme.com/docs/colors-and-palettes
+  SNOW_STORM_1 = '#d8dee9',
+  SNOW_STORM_2 = '#e5e9f0',
+  SNOW_STORM_3 = '#eceff4',
+}
