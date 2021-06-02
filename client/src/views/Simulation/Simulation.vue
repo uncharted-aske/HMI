726c89af9ad662810f61476445d5e6772d925e24
Manfred Cheung <mj3cheun@uwaterloo.ca>
2021-06-02 13:28:38 -0400

    Fix lint errors

--- client/src/views/Simulation/Simulation.vue
+++ client/src/views/Simulation/Simulation.vue
@@ -45,7 +45,7 @@
               <i class="fas fa-expand-alt"/>
             </button>
           </settings-bar>
-          <global-graph v-if="selectedModel" :data="selectedGraph" @node-click="onNodeClick"/>
+          <global-graph v-if="selectedModel" :data="selectedGraph"/>
         </div>
         <div slot="parameters" class="h-100 w-100 d-flex flex-column">
           <settings-bar>
@@ -63,7 +63,7 @@
               </button>
             </div>
           </settings-bar>
-          <global-graph v-if="selectedModel" :data="selectedGraph" @node-click="onNodeClick"/>
+          <global-graph v-if="selectedModel" :data="selectedGraph"/>
         </div>
         <div slot="variables" class="h-100 w-100 d-flex flex-column">
           <settings-bar>
@@ -81,7 +81,7 @@
               </button>
             </div>
           </settings-bar>
-          <global-graph v-if="selectedModel" :data="selectedGraph" @node-click="onNodeClick"/>
+          <global-graph v-if="selectedModel" :data="selectedGraph"/>
         </div>
       </resizable-grid>
     </div>
@@ -95,7 +95,7 @@
   import { RawLocation } from 'vue-router';
 
   import { TabInterface, ViewInterface, ModelInterface } from '@/types/types';
-  import { GraphInterface, GraphNodeInterface } from '@/types/typesGraphs';
+  import { GraphInterface } from '@/types/typesGraphs';
 
   import { NodeTypes } from '@/graphs/svg/encodings';
 
@@ -120,234 +120,7 @@
   /**
   Temporary hack for workshop
   **/
-  const bakedData = {
-    success: {
-      v: 1,
-      next_page: '',
-      scrollId: '',
-      hits: 1,
-      data: [{
-        pubname: 'Chaos, Solitons & Fractals',
-        publisher: 'Elsevier',
-        _gddid: '5ef5fd21a58f1dfd520aec60',
-        title: 'CHIME: COVID-19 Hospital Impact for Epidemics - Online Documentation',
-        doi: '',
-        coverDate: '2021-01-19T21:08',
-        URL: 'https://drive.google.com/file/d/122LEBSEMF9x-3r3tWbF6YQ9xeV4I_9Eh/view?usp=sharing',
-        authors: [{ name: 'Jason Lubken' },
-                  { name: 'Marieke Jackson' },
-                  { name: 'Michael Chow' }],
-        highlight: ['carriers, our analysis estimates the value of the <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>', 'estimates the value of the <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> (R0 ) as', '<em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> (R0 ) as of May 11, 2020 was found to be', '0) 0  181 182 183 184 185 186 187  188 189  190 191  3.2. <em class="hl">Basic</em> <em class="hl">Reproduction</em> <em class="hl">Number</em>', '0) 0  181 182 183 184 185 186 187  188 189  190 191  3.2. <em class="hl">Basic</em> <em class="hl">Reproduction</em> <em class="hl">Number</em> for Proposed', '<em class="hl">Basic</em> <em class="hl">Reproduction</em> <em class="hl">Number</em> for Proposed Model Using the next generation', '= −σ1 k γ + φD −σ2 k 0 φU + δU   The associated <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>,', '+ δU   The associated <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>, denoted', '<em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>, denoted by R0 is then given by, R0 = ρ (FV', 'represents the robustness of the model forecasting. The <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>', 'model forecasting. The <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> is 4.234', '<em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> is 4.234 as of May 08, which lies in prior', 'peak around June 11 with about 26.449K cases. The <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>', 'about 26.449K cases. The <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> is estimated', '<em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> is estimated about 5.3467 as of May 11, which', 'about 5.3467 as of May 11, which is in between the observed <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> for', 'in between the observed <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> for COVID-19,', '<em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> for COVID-19, estimated about 2-7 for COVID-19', 'peak around June 15 with about 9.504K cases. The <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>', 'about 9.504K cases. The <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> is 5.218', '<em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> is 5.218 as of May 11, which lies between', 'of COVID-19 dynamics. According to our calculation, the <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>', 'to our calculation, the <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> is around', '<em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> is around 4.649 as of May 09, which lies', 'control the disease burden of COVID-19. Otherwise, this <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em>', 'COVID-19. Otherwise, this <em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> could increase', '<em class="hl">basic</em> <em class="hl">reproduction</em> <em class="hl">number</em> could increase upto 5.7 within 20 days and'],
-      }],
-    },
-  };
 
-  const bibjson = {
-    bibjson: {
-      title: 'CHIME: COVID-19 Hospital Impact for Epidemics - Online Documentation',
-      author: [
-        { name: 'Jason Lubken' },
-        { name: 'Marieke Jackson' },
-        { name: 'Michael Chow' },
-      ],
-      type: 'web_documentation',
-      link: [{ url: 'https://code-for-philly.gitbook.io/chime/' }],
-      year: '2021-01-19T21:08',
-      file: 'CHIME-online-manual-T2021-01-19.pdf',
-      file_url: 'https://drive.google.com/file/d/122LEBSEMF9x-3r3tWbF6YQ9xeV4I_9Eh/view?usp=sharing',
-      identifier: [{ type: 'aske_id', id: '8467496e-3dfb-4efd-9061-433fef1b92de' }],
-    },
-  };
-
-  const paths = [{
-                   nodes: [{
-                     id: '3ca29049-d9c6-459e-be33-5912e8a1433b',
-                   }, {
-                     id: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                   }],
-                   edges: [{
-                     source: '3ca29049-d9c6-459e-be33-5912e8a1433b',
-                     target: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                   }],
-                 },
-                 {
-                   nodes: [{
-                             id: '3ca29049-d9c6-459e-be33-5912e8a1433b',
-                           },
-                           {
-                             id: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                           }, {
-                             id: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                           },
-                   ],
-                   edges: [{
-                             source: '3ca29049-d9c6-459e-be33-5912e8a1433b',
-                             target: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                           },
-                           {
-                             source: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                             target: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                           },
-                   ],
-                 },
-                 {
-                   nodes: [{
-                             id: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                           },
-                           {
-                             id: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                           },
-                           {
-                             id: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                           },
-                           {
-                             id: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                           },
-                           {
-                             id: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                           },
-                           {
-                             id: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                           },
-                   ],
-                   edges: [{
-                             source: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                             target: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                           },
-                           {
-                             source: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                             target: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                           },
-                           {
-                             source: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                             target: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                           },
-                           {
-                             source: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                             target: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                           },
-                           {
-                             source: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                             target: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                           },
-                   ],
-                 },
-                 {
-                   nodes: [{
-                             id: '3ca29049-d9c6-459e-be33-5912e8a1433b',
-                           },
-                           {
-                             id: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                           },
-                           {
-                             id: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                           },
-                           {
-                             id: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                           },
-                           {
-                             id: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                           },
-                           {
-                             id: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                           },
-                           {
-                             id: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                           },
-                           {
-                             id: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                           },
-                           {
-                             id: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                           },
-                   ],
-                   edges: [
-                     {
-                       source: '3ca29049-d9c6-459e-be33-5912e8a1433b',
-                       target: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                     },
-
-                     {
-                       source: '3ca29049-d9c6-459e-be33-5912e8a1433b',
-                       target: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                     }, {
-                       source: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                       target: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                     }, {
-                       source: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                       target: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                     }, {
-                       source: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                       target: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                     }, {
-                       source: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                       target: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                     }, {
-                       source: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                       target: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                     }, {
-                       source: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                       target: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                     },
-                   ],
-                 },
-                 {
-                   nodes: [{
-                     id: '4f8f229a-aab4-4db4-844b-64f68920dfad',
-                   }, {
-                     id: '9d85aa4e-2a80-42a9-a8fb-33d10932f2ac',
-                   }, {
-                     id: '4a3659ff-9c44-4352-9d3e-f592b2597b5a',
-                   }, {
-                     id: '26f0ec12-51a6-42e5-a5fd-afdad6d21f4e',
-                   }, {
-                     id: 'a9840a0e-8a1a-4eda-a9ca-7746f23d3677',
-                   }, {
-                     id: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                   }, {
-                     id: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                   }, {
-                     id: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                   }, {
-                     id: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                   }, {
-                     id: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                   }, {
-                     id: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                   }],
-                   edges: [{
-                     source: '4f8f229a-aab4-4db4-844b-64f68920dfad',
-                     target: '9d85aa4e-2a80-42a9-a8fb-33d10932f2ac',
-                   }, {
-                     source: '9d85aa4e-2a80-42a9-a8fb-33d10932f2ac',
-                     target: '4a3659ff-9c44-4352-9d3e-f592b2597b5a',
-                   }, {
-                     source: '4a3659ff-9c44-4352-9d3e-f592b2597b5a',
-                     target: '26f0ec12-51a6-42e5-a5fd-afdad6d21f4e',
-                   }, {
-                     source: '26f0ec12-51a6-42e5-a5fd-afdad6d21f4e',
-                     target: 'a9840a0e-8a1a-4eda-a9ca-7746f23d3677',
-                   }, {
-                     source: 'a9840a0e-8a1a-4eda-a9ca-7746f23d3677',
-                     target: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                   }, {
-                     source: '4aa6aa7d-31bc-4c43-9462-087640c2bc3f',
-                     target: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                   }, {
-                     source: 'aa5a9672-d3f7-4c4a-95e2-d0a1a77197d0',
-                     target: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                   }, {
-                     source: '475fca7c-194c-4abb-b19b-0aa28a0bb5d3',
-                     target: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                   }, {
-                     source: 'b33a5e7a-35f3-4ec0-a9f4-b4b57e4bd524',
-                     target: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                   }, {
-                     source: 'a414982f-51fb-4d41-abf8-b736e9fc6ac1',
-                     target: '9d2259db-62cc-444f-a4d4-03f5769ba39b',
-                   }],
-                 },
-  ];
   const components = {
     SettingsBar,
     Counters,
@@ -455,11 +228,6 @@
       }
 
       this.$router.push(options);
-    }
-
-    onNodeClick (node: GraphNodeInterface): void {
-      const nodeMetadata = node.metadata;
-      console.log(node);
     }
   }
 </script>
