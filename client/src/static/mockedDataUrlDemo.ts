e9fb3bd9713e1fdad3488cef6872a82c8f428107
RosaRomeroGomez <rgomez@uncharted.software>
2021-06-22 11:53:10 -0400

    renamed file

---
+++ client/src/static/mockedDataUrlDemo.ts
@@ -0,0 +1,4 @@
+export const staticFileURLs = [
+    `${window.location.origin}/gromets/SimpleSIR_gromet_PetriNetClassic_metadata.json`,
+    `${window.location.origin}/gromets/SimpleSIR_gromet_FunctionNetwork_metadata.json`,
+];
