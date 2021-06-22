dba3bc7c741d5a8f46625d205328686b7523705b
RosaRomeroGomez <rgomez@uncharted.software>
2021-06-22 14:44:50 -0400

    removed

--- client/src/static/mockedDataUrlDemo.ts
+++
@@ -1,4 +0,0 @@
-export const staticFileURLs = [
-    `${window.location.origin}/gromets/SimpleSIR_gromet_PetriNetClassic_metadata.json`,
-    `${window.location.origin}/gromets/SimpleSIR_gromet_FunctionNetwork_metadata.json`,
-];
