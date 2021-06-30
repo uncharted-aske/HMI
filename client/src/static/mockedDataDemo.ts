ab024b4507118b5db859b20cb777a7bbc218ee15
RosaRomeroGomez <rgomez@uncharted.software>
2021-06-30 12:05:07 -0400

    added seir PN classic model

--- client/src/static/mockedDataDemo.ts
+++ client/src/static/mockedDataDemo.ts
@@ -7,12 +7,15 @@ import { getUtil } from '@/utils/FetchUtil';
 export const staticFileURLs = [
   `${window.location.origin}/gromets/SimpleSIR_gromet_PetriNetClassic_metadata.json`,
   `${window.location.origin}/gromets/SimpleSIR_gromet_FunctionNetwork_metadata.json`,
+  `${window.location.origin}/gromets/SEIR_PetriNetClassic.json`,
 ];
 
 // eslint-disable-next-line
-export const buildInitialModelsList = ({ SIR_PN, SIR_FN }: any): Model.Model[] => {
+export const buildInitialModelsList = ({ SIR_PN, SIR_FN, SEIR_PN }: any): Model.Model[] => {
   const PN = GroMEt2Graph.parseGromet(SIR_PN);
   const FN = GroMEt2Graph.parseGromet(SIR_FN);
+  const SEIR_PN_PARSED = GroMEt2Graph.parseGromet(SEIR_PN);
+
   return [
     {
       id: 0,
@@ -42,12 +45,40 @@ export const buildInitialModelsList = ({ SIR_PN, SIR_FN }: any): Model.Model[] =
         },
       ],
     },
+    {
+      id: 1,
+      name: SEIR_PN.name, // name should be the same across modeling frameworks
+      metadata: {
+        name: SEIR_PN.name,
+        description: '', // We are going to need a high-level description for each model.
+      },
+      modelGraph: [
+        {
+          file: '',
+          type: Model.GraphTypes.PetriNetClassic,
+          metadata: SEIR_PN.metadata,
+          graph: {
+            edges: SEIR_PN_PARSED.edges as Graphs.GraphEdgeInterface[],
+            nodes: SEIR_PN_PARSED.nodes as unknown as Graphs.GraphNodeInterface[],
+          },
+        },
+        // {
+        //   file: '',
+        //   type: Model.GraphTypes.FunctionNetwork,
+        //   metadata: FN.metadata,
+        //   graph: {
+        //     edges: FN.edges as Graphs.GraphEdgeInterface[],
+        //     nodes: FN.nodes as unknown as Graphs.GraphNodeInterface[],
+        //   },
+        // },
+      ],
+    },
   ];
 };
 
 export const fetchInitialModelData = async (): Promise<Model.Model[]> => {
-  const [SIR_PN, SIR_FN] = await Promise.all(
+  const [SIR_PN, SIR_FN, SEIR_PN] = await Promise.all(
     staticFileURLs.map(url => getUtil(url)),
   );
-  return buildInitialModelsList({ SIR_PN, SIR_FN });
+  return buildInitialModelsList({ SIR_PN, SIR_FN, SEIR_PN });
 };
