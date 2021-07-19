13ba09629b628c4cb946cd524f7fcfdb4368e8ab
Manfred Cheung <mj3cheun@uwaterloo.ca>
2021-07-19 13:40:29 -0400

    Switch model representation highlighting from using colour encoding to opacity

--- client/src/views/Models/components/Graphs/GlobalGraph.vue
+++ client/src/views/Models/components/Graphs/GlobalGraph.vue
@@ -30,7 +30,6 @@
   export default class GlobalGraph extends Vue {
     @Prop({ default: null }) data: GraphInterface;
     @Prop({ default: null }) subgraph: SubgraphInterface;
-    @Prop({ default: null }) highlighted: SubgraphInterface;
     @Prop({ default: GraphLayoutInterfaceType.elk }) layout: string;
 
     renderingOptions = DEFAULT_RENDERING_OPTIONS;
@@ -39,7 +38,6 @@
     @Watch('data')
     dataChanged (): void {
       this.refresh();
-      this.renderer.highlightSubgraph(this.highlighted);
     }
 
     @Watch('subgraph')
@@ -51,12 +49,6 @@
     async layoutChanged (): Promise<void> {
       await this.refresh();
       this.subgraphChanged();
-      this.renderer.highlightSubgraph(this.highlighted);
-    }
-
-    @Watch('highlighted')
-    async highlightedChanged (): Promise<void> {
-      this.renderer.highlightSubgraph(this.highlighted);
     }
 
     subgraphChanged (): void {
@@ -83,14 +75,17 @@
       });
 
       this.renderer.setCallback('nodeClick', (evt, node) => {
+        if (this.subgraph) {
+          this.renderer.showSubgraph(this.subgraph);
+          return;
+        }
+
         this.renderer.hideSubgraph();
 
         if (node.datum().nodes) {
           const id = node.datum().id;
           if (node.datum().collapsed === true) {
             this.renderer.expand(id);
-            // Wait for renderer.expand to finish async updates before calling highlightSubgraph
-            setTimeout(() => this.renderer.highlightSubgraph(this.highlighted), 500);
           } else {
             this.renderer.collapse(id);
           }
@@ -118,9 +113,11 @@
       });
 
       this.renderer.setCallback('backgroundClick', () => {
-        this.renderer.hideSubgraph();
-        this.renderer.clearSelections();
-        this.$emit('background-click');
+        if (!this.subgraph) {
+          this.renderer.hideSubgraph();
+          this.renderer.clearSelections();
+          this.$emit('background-click');
+        }
       });
 
       this.refresh();
