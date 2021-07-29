488b66a834495427949ff72f68ec295db2c13771
RosaRomeroGomez <rgomez@uncharted.software>
2021-07-29 16:19:36 -0400

    added new message display component

---
+++ client/src/components/widgets/MessageDisplay.vue
@@ -0,0 +1,35 @@
+<template>
+  <div 
+    class="alert m-3"     
+    :class="alertType">
+   {{message}}
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import { Prop } from 'vue-property-decorator';
+  
+  @Component
+  export default class Counters extends Vue {
+    @Prop({ default: '' })
+    message: string;
+    @Prop({ default: 'info' })
+    messageType: string;
+
+    get alertType (): string {
+      if (this.messageType === 'warning') {
+        return 'alert-warning';
+      } else if (this.messageType === 'primary') {
+        return 'alert-primary';
+      }
+      return 'alert-info';
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
