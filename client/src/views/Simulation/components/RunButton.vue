5e8b16e266f2ca908dcdb91c1a00309297122b58
Yohann Paris <github@yohannparis.com>
2021-08-03 16:23:06 -0400

    Change from dropdown to modal box

--- client/src/views/Simulation/components/RunButton.vue
+++ client/src/views/Simulation/components/RunButton.vue
@@ -1,38 +1,35 @@
 <template>
-  <div class="btn-group">
+  <div class="run-button btn-group">
     <button
       type="button"
       class="btn btn-primary blue"
       :disabled="disabled"
       @click="onClickRun"
     >
-      <font-awesome-icon :icon="['fas', ( autoRunInput ? 'pause' : 'play') ]" />
+      <font-awesome-icon :icon="['fas', displayIcon ]" />
       {{ displayText }}
     </button>
     <button
       type="button"
-      class="btn btn-primary blue dropdown-toggle dropdown-toggle-split"
-      :class="{ 'open': dropdownOpen }"
-      @click="dropdownOpen = !dropdownOpen"
-    />
-    <div class="dropdown-menu dropdown-menu-right" :class="{ 'show': dropdownOpen }">
-      <input type="checkbox" v-model="autoRunInput" id="auto-run-input" />
-      <label
-        for="auto-run-input"
-        title="Enable model to run automatically on when Parameters are updated">
-        Auto-Run
-      </label>
-      <div class="dropdown-divider" />
-      <h6 class="dropdown-header">Model Execution Settings</h6>
-      <div class="run-config">
-        <label for="run-config-start">Start</label>
-        <input type="number" id="run-config-start" v-model.number="configInput.start" />
-        <label for="run-config-end">End</label>
-        <input type="number" id="run-config-end" v-model.number="configInput.end" />
-        <label for="run-config-step">Step</label>
-        <input type="number" id="run-config-step" v-model.number="configInput.step" />
-      </div>
-    </div>
+      class="btn btn-primary"
+      @click="settingOpen = !settingOpen"
+    >
+      <font-awesome-icon :icon="['fas', 'cog' ]" />
+    </button>
+
+    <modal v-if="settingOpen" @close="settingOpen = false">
+      <h5 class="header" slot="header">Model Execution Settings</h5>
+      <section slot="body" class="run-config">
+        <label for="auto-run-input" title="Enable model to run automatically on when Parameters are updated">Auto-Run</label>
+        <input type="checkbox" v-model="autoRunInput" id="auto-run-input" />
+        <label for="start">Start</label>
+        <input type="number" id="start" v-model.number="configInput.start" />
+        <label for="end">End</label>
+        <input type="number" id="end" v-model.number="configInput.end" />
+        <label for="step">Step</label>
+        <input type="number" id="step" v-model.number="configInput.step" />
+      </section>
+    </modal>
   </div>
 </template>
 
@@ -41,17 +38,26 @@
   import Component from 'vue-class-component';
   import { Prop, PropSync } from 'vue-property-decorator';
   import * as Donu from '@/types/typesDonu';
+  import Modal from '@/components/Modal.vue';
 
-  @Component
+  const components = {
+    Modal,
+  };
+
+  @Component({ components })
   export default class RunButton extends Vue {
     @PropSync('autoRun', { type: Boolean }) autoRunInput!: boolean;
     @PropSync('config', { type: Object }) configInput!: Donu.RequestConfig;
     @Prop({ default: false }) disabled: boolean;
 
-    dropdownOpen: boolean = false;
+    settingOpen: boolean = false;
 
     get displayText (): string {
-      return this.autoRunInput ? 'Auto' : 'Run ';
+      return this.autoRunInput ? 'Auto-Run' : 'Run ';
+    }
+
+    get displayIcon (): string {
+      return this.autoRunInput ? 'pause' : 'play';
     }
 
     /* On auto-run, remove the auto-run feature, otherwise, run the simulation. */
@@ -66,40 +72,29 @@
 </script>
 
 <style scoped>
-  button:first-of-type {
-    width: 5em;
-  }
-
-  .dropdown-menu {
-    padding: 1em;
-  }
-
-  .dropdown-header {
-    padding: 0 1.5em .5em 0;
-  }
-
-  /* Make the caret bigger */
-  .dropdown-toggle {
-    font-size: 1.5em;
-    padding: 0 .5625rem; /* Bootstap thingy */
-  }
-
-  /* Reverse the caret when open */
-  .dropdown-toggle.open::after {
-    border-bottom: 0.3em solid;
-    border-top: 0;
+  .header {
+    margin-bottom: 0;
   }
 
   .run-config {
+    align-items: center;
     display: grid;
     font-size: .9em;
     gap: .25em;
-    grid-template-columns: 3em auto;
+    grid-template-columns: 5em auto;
+    justify-items: start;
   }
 
   /* Align the label with the inputs */
   .run-config label {
     line-height: 2;
     margin: 0;
+  }
+
+  /* Overide modal styling */
+  .run-button::v-deep .modal-container {
+    height: auto;
+    max-height: 90vh;
+    width: auto;
   }
 </style>
