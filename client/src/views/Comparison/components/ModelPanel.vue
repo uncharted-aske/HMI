5c934191fd62ff0b1c3f1814a23ec64e44de714a
RosaRomeroGomez <rgomez@uncharted.software>
2021-08-06 07:05:13 -0400

    added model panel for model comparison

---
+++ client/src/views/Comparison/components/ModelPanel.vue
@@ -0,0 +1,132 @@
+<template>
+  <section class="simulation-model-panel">
+    <settings-bar>
+      <counters slot="left" :title="modelName" :data="countersData" />
+    </settings-bar>
+    <global-graph
+      v-if="graph"
+      :data="graph"
+      :displayed-nodes="displayedNodes"
+      @node-click="onNodeClick"
+      @background-click="onBackgroundClick"
+      @node-dblclick="onNodeDblClick"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import { Prop } from 'vue-property-decorator';
+  import { Action, Getter } from 'vuex-class';
+  import * as HMI from '@/types/types';
+  import * as Model from '@/types/typesModel';
+  import * as Graph from '@/types/typesGraphs';
+  import Counters from '@/components/Counters.vue';
+  import SettingsBar from '@/components/SettingsBar.vue';
+  import GlobalGraph from '@/views/Models/components/Graphs/GlobalGraph.vue';
+
+  const components = {
+    Counters,
+    GlobalGraph,
+    SettingsBar,
+  };
+
+  @Component({ components })
+  export default class ModelPanel extends Vue {
+    @Prop({ default: false }) expanded: boolean;
+    @Prop({ default: null }) model: Model.Model;
+
+    @Getter getSimModel;
+    @Getter getSelectedModelGraphType;
+    @Action toggleParameter;
+    @Action toggleVariable;
+
+    settingsOpen: boolean = false;
+
+    onNodeClick (selected: Graph.GraphNodeInterface): void {
+      this.$emit('highlight', selected.label);
+    }
+
+    onBackgroundClick (): void {
+      this.$emit('highlight', '');
+    }
+
+    onNodeDblClick (selected: Graph.GraphNodeInterface): void {
+      this.toggleParameter({ modelId: this.model.id, selector: selected.label });
+      this.toggleVariable({ modelId: this.model.id, selector: selected.label });
+    }
+
+    get parameters (): HMI.SimulationParameter[] {
+      return this.getSimModel(this.model.id).parameters;
+    }
+
+    get variables (): HMI.SimulationVariable[] {
+      return this.getSimModel(this.model.id).variables;
+    }
+
+    get displayedNodes (): Graph.SubgraphNodeInterface[] {
+      const nodes = this.graph?.nodes;
+      if (nodes) {
+        const displayedLabels = [
+          ...this.parameters
+              .filter(parameter => parameter.displayed)
+              .map(parameter => parameter.metadata.name),
+          ...this.variables
+              .filter(variable => variable.displayed)
+              .map(variable => variable.metadata.name),
+        ];
+
+        if (displayedLabels.length > 0) {
+          return nodes
+            .filter(node => displayedLabels.includes(node.label))
+            .map(node => ({ id: node.id }));
+        }
+      }
+      return [];
+    }
+
+    get graph (): Graph.GraphInterface {
+      const selectedModelGraph = this.model?.modelGraph.find(graph => {
+        return graph.type === this.getSelectedModelGraphType;
+      });
+      return selectedModelGraph?.graph ?? null;
+    }
+
+    get modelName (): string {
+      return this.model?.metadata?.name;
+    }
+
+    get countersData (): HMI.Counter[] {
+      const data = [];
+      if (this.parameters.length > 0) {
+        data.push({
+          name: 'Parameters',
+          value: this.parameters.length,
+        });
+      }
+      if (this.variables.length > 0) {
+        data.push({
+          name: 'Variables',
+          value: this.variables.length,
+        });
+      }
+      return data;
+    }
+  }
+</script>
+
+<style scoped>
+  .settings-bar-container aside {
+    display: flex;
+    gap: .3em;
+  }
+
+  .dropdown-menu {
+    padding: 1em;
+  }
+
+  .dropdown-header {
+    padding: 0 1.5em .5em 0;
+  }
+</style>
