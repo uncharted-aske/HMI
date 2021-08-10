bb32001b58bc85cb37f07f3b3855c2f8f18ff7a0
Yohann Paris <github@yohannparis.com>
2021-08-10 16:29:03 -0400

    Make the graph legend a two part component for more reusability

---
+++ client/src/components/widgets/LegendPanel.vue
@@ -0,0 +1,85 @@
+<template>
+  <aside class="legend-panel" :class="{ 'open' : isOpen }">
+    <header>
+      <button type="button" @click="toggleLegend()">
+        Legend
+        <font-awesome-icon :icon="['fas', toggleIcon ]" />
+      </button>
+    </header>
+    <div class="legends">
+      <slot />
+    </div>
+  </aside>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import { Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class LegendPanel extends Vue {
+    @Prop({ default: false }) open: boolean;
+    isOpen: boolean = false;
+
+    mounted (): void {
+      this.isOpen = Boolean(this.open);
+    }
+
+    get toggleIcon (): string {
+      return this.isOpen ? 'angle-double-down' : 'angle-double-up';
+    }
+
+    toggleLegend (): void {
+      this.isOpen = !this.isOpen;
+    }
+  }
+</script>
+
+<style scoped>
+  .legend-panel {
+    --background-colour: white;
+    background-color: var(--background-colour);
+    border: var(--border);
+    border-radius: 5px;
+    bottom: .5em;
+    left: 1em;
+    overflow: hidden;
+    position: absolute;
+  }
+
+  header {
+    padding: .33em 0;
+    text-align: center;
+  }
+
+  header button {
+    background-color: var(--background-colour);
+    border: none;
+    font-size: .85em;
+    font-weight: bold;
+    line-height: 1rem;
+    outline: none;
+    text-transform: uppercase;
+  }
+
+  .legends {
+    display: flex;
+    flex-direction: row;
+  }
+
+  /* isOpen */
+  .legends {
+    max-height: 0;
+    max-width: 0;
+    padding: 0;
+    transition: max-height 100ms, max-width 100ms, padding 50ms;
+    will-change: max-height, max-width, padding;
+  }
+  .legend-panel.open .legends {
+    max-height: 100vh;
+    max-width: 100vw;
+    padding: 1em;
+    transition: max-height 500ms, max-width 500ms, padding 50ms;
+  }
+</style>
